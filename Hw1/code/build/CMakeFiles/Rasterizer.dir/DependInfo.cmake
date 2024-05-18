adcdf27452e4b5da593d0c753759c4988a0fd811
fyq <1210915690@qq.com>
2024-05-18 16:06:34 +0800

    fix hw1

--- Hw1/code/build/CMakeFiles/Rasterizer.dir/DependInfo.cmake
+++ Hw1/code/build/CMakeFiles/Rasterizer.dir/DependInfo.cmake
@@ -1,16 +1,18 @@
-
-# Consider dependencies only in project.
-set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
-
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
   )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/usr/games/CG/Hw1/code/Triangle.cpp" "/usr/games/CG/Hw1/code/build/CMakeFiles/Rasterizer.dir/Triangle.cpp.o"
+  "/usr/games/CG/Hw1/code/main.cpp" "/usr/games/CG/Hw1/code/build/CMakeFiles/Rasterizer.dir/main.cpp.o"
+  "/usr/games/CG/Hw1/code/rasterizer.cpp" "/usr/games/CG/Hw1/code/build/CMakeFiles/Rasterizer.dir/rasterizer.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
 
-# The set of dependency files which are needed:
-set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "/usr/games/CG/Hw1/code/Triangle.cpp" "CMakeFiles/Rasterizer.dir/Triangle.cpp.o" "gcc" "CMakeFiles/Rasterizer.dir/Triangle.cpp.o.d"
-  "/usr/games/CG/Hw1/code/main.cpp" "CMakeFiles/Rasterizer.dir/main.cpp.o" "gcc" "CMakeFiles/Rasterizer.dir/main.cpp.o.d"
-  "/usr/games/CG/Hw1/code/rasterizer.cpp" "CMakeFiles/Rasterizer.dir/rasterizer.cpp.o" "gcc" "CMakeFiles/Rasterizer.dir/rasterizer.cpp.o.d"
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include/opencv4"
   )
 
 # Targets to which this target links.
--- Hw1/code/build/CMakeFiles/Rasterizer.dir/cmake_clean.cmake
+++ Hw1/code/build/CMakeFiles/Rasterizer.dir/cmake_clean.cmake
@@ -1,10 +1,7 @@
 file(REMOVE_RECURSE
   "CMakeFiles/Rasterizer.dir/Triangle.cpp.o"
-  "CMakeFiles/Rasterizer.dir/Triangle.cpp.o.d"
   "CMakeFiles/Rasterizer.dir/main.cpp.o"
-  "CMakeFiles/Rasterizer.dir/main.cpp.o.d"
   "CMakeFiles/Rasterizer.dir/rasterizer.cpp.o"
-  "CMakeFiles/Rasterizer.dir/rasterizer.cpp.o.d"
   "Rasterizer"
   "Rasterizer.pdb"
 )
